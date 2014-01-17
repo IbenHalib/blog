4ad0acce24eeb30a13c75ebb1a818ad6c0e3b9bb
vadim <vadim_2991@ukr.net>
2014-01-18 00:39:30 +0200

    v8

--- src/Vadim/GuestBundle/Resources/public/js/ajax.js
+++ src/Vadim/GuestBundle/Resources/public/js/ajax.js
@@ -2,6 +2,6 @@ $(document).ready(function(){
     var i=1;
     $("#but1").click(function(){
         i++;
-        $("#"+i).load("http://blog/showArticles/"+i);
+        $("#"+i).load("http://guest/posts/"+i);
     });
 });
