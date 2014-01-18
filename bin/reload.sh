71ffd8eea96c8156d4aaad721c6045e7bb860e3b
vadim <vadim_2991@ukr.net>
2014-01-18 05:33:35 +0200

    v10

--- bin/reload.sh
+++ bin/reload.sh
@@ -6,18 +6,8 @@ cd ..
 set -x
 curl -s https://getcomposer.org/installer | php
 php composer.phar install
+
 php app/console doctrine:database:drop --force
-
-find app/cache/ -type d -maxdepth 1 -mindepth 1 | while read L; do
-    rm -rf "$L"
-done
-
-rm -rf web/bundles
-rm -rf web/css
-rm -rf web/fonts
-rm -rf web/images
-rm -rf web/js
-
 php app/console doctrine:database:create
 php app/console doctrine:schema:update --force
 
