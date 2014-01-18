af218bfbe3b4626e358b15743947d5e8ed4b12b9
vadim <vadim_2991@ukr.net>
2014-01-18 05:15:54 +0200

    v10

--- bin/reload.sh
+++ bin/reload.sh
@@ -18,7 +18,7 @@ rm -rf web/images
 rm -rf web/js
 
 php app/console doctrine:database:create
-php app/console doctrine:schema:create
+php app/console doctrine:schema:update --force
 
 php app/console braincrafted:bootstrap:install
 php app/console assets:install --symlink
